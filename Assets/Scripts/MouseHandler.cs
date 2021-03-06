22a94e1f5596b59a5e7cc07a64a45dc56520d71c
SubsTheTechnomancer <54163143+SubsTheTechnomancer@users.noreply.github.com>
2021-03-06 17:44:45 +0530

    Started Project, basic movement and cam

---
+++ Assets/Scripts/MouseHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHandler : MonoBehaviour
+{
+    
+    public float horizontalSpeed = 1f;
+    public float verticalSpeed = 1f;
+    public Transform player;
+    
+    private float xRotation = 0.0f, yRotation = 0.0f;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X")*horizontalSpeed;
+        float mouseY = Input.GetAxis("Mouse Y")*verticalSpeed;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,-90,90);
+
+        cam.transform.eulerAngles = new Vector3(xRotation,yRotation,0.0f);
+        player.eulerAngles = new Vector3(0,yRotation,0);
+    }
+}
---
+++ Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour
+{
+    
+    CharacterController character;
+    
+    public float MovementSpeed = 1;
+    public float Gravity = 9.8f;
+    public float velocity = 0;
+
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal")*MovementSpeed;
+        float vertical = Input.GetAxis("Vertical")*MovementSpeed;
+        character.Move((transform.right*horizontal+transform.forward*vertical)*Time.deltaTime);
+
+        if(character.isGrounded)
+            velocity = 0;
+        else{
+            velocity -= Gravity*Time.deltaTime;
+            character.Move(new Vector3(0,velocity,0));
+        }   
+    }
+}
